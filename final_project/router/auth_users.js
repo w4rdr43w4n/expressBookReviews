c976d8fc94150ded79f6c63ec37e9a40cccf1ea2
w4rdr43w4n <zwerdos123@gmail.com>
2025-04-05 06:04:07 -0400

    task0

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,8 +5,8 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username)=>{ 
+    
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
